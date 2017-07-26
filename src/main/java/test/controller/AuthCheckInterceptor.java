6dbe7128530941263a6da8e39a52898332e00a08
Gvo3d <denis.yakimov89@gmail.com>
2017-07-26 22:06:29 +0300

    +

--- src/main/java/test/controller/AuthCheckInterceptor.java
+++ src/main/java/test/controller/AuthCheckInterceptor.java
@@ -5,6 +5,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 
 /**
  * Created by Gvozd on 11.12.2016.
@@ -17,7 +18,15 @@ public class AuthCheckInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println();
         System.out.println("POSTHANDLE");
+        System.out.println("Headers:");
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            System.out.println(key+": "+value);
+        }
         System.out.println("Request URI: "+request.getRequestURI());
         System.out.println("REFERER: "+request.getHeader("referer"));
         System.out.println("RESPONSE VIEW: "+modelAndView.getViewName());
